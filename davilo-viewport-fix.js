94b784221bde24845e7aa7c973906e5edbd398b2
Mamrez <mrz_shirazi@yahoo.com>
2020-09-23 16:33:52 +0330

    davilo-viewport-fix.js another try!!

--- davilo-viewport-fix.js
+++ davilo-viewport-fix.js
@@ -1,3 +1,3 @@
 $(window).on('load', function() {
-    document.querySelector('meta[name="viewport"]').setAttribute("content", "width=deivce-width initial-scale=1.0");
+    document.querySelector('meta[name="viewport"]').content = "width=device-width initial-scale=1.0";
 });
